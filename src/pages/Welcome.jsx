196e3524c4ad448fa53969aa968235f10d290491
PermanentPig <791612172@qq.com>
2019-07-24 19:35:39 +0800

    测试下提交代码，确认当前分支

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -9,8 +9,8 @@ export default () => (
     <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
       use block
     </a>
-    。
     <h2>添加一个测试</h2>
     <h2>添加第二个测试</h2>
+    <p>添加第三个擦拭</p>
   </p>
 );
